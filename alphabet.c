f552c7e31a5faaee87545c05c5cb3a845c820080
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-01-18 11:31:48 +0000

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	char let;
+	scanf("%c",&let);
+	if(let=='A'||let=='S'||let=='D'let=='F'||let=='G'||let=='H'||let=='J'||let=='K'||let=='L'||let=='P'||
+	let=='O'||let=='I'||let=='U'let=='Y'||let=='T'||let=='R'||let=='E'||let=='W'||let=='Q'||let=='Z'||
+	let=='X'||let=='C'||let=='V'let=='B'||let=='N'||let=='M'||let=='q'||let=='w'||let=='e'||let=='r'||let=='t'||let=='y'||let=='u'let=='i'||let=='o'||let=='p'||let=='l'||let=='k'||let=='j'||let=='h'||
+	let=='g'||let=='f'||let=='d'let=='s'||let=='a'||let=='z'||let=='x'||let=='c'||let=='v'||let=='b'||let=='n'||let=='m'
+	)
+	{
+		printf("Alphabet");
+	}
+	else
+	{
+		printf("Not alphabet");
+	}
+	
+	return 0;
+}
